94ed2731e0b7d2d21a9edaf30cf1e4405010cb19
romaric.guth <romaric.guth@epita.fr>
2021-10-20 12:42:04 +0200

    exercice 2

---
+++ day-1/exercice-2.js
@@ -0,0 +1,12 @@
+import {my_display_alpha} from "./exercice-1.js";
+
+export const my_display_alpha_reverse = () => {
+    const alpha = my_display_alpha();
+    let alpha_reverse = '';
+    for (let i = alpha.length - 1; i >= 0; i--) {
+        alpha_reverse += alpha[i];
+    }
+    return alpha_reverse;
+}
+
+console.log(my_display_alpha_reverse());
