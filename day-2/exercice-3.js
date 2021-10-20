deb16c4da9ec38e8f5a318d251766da7f798b411
romaric.guth <romaric.guth@epita.fr>
2021-10-20 15:23:47 +0200

    exercice 2-3

--- day-2/exercice-3.js
+++ day-2/exercice-3.js
@@ -1,12 +1,42 @@
-export const my_alpha_reverse = (str) => {
+export const my_string_is_number = (str) => {
   if (typeof str !== 'string') {
-    return '';
+    return false;
   }
 
-  let str_reverse = '';
-  for (let i = str.length - 1; i >= 0; i--) {
-    str_reverse += str[i];
+  let i = 0;
+  let has_number = false;
+  let has_dot = false;
+  while (str[i] === ' ') {
+    i++;
   }
-  return str_reverse;
+
+  if (str[i] === '-' || str[i] === '+') {
+    i++;
+  }
+
+  while (str[i] !== undefined) {
+    if (!has_dot && str[i] === ',' || str[i] === '.') {
+      has_dot = true;
+    } else if (str.charCodeAt(i) >= 48 && str.charCodeAt(i) <= 57) {
+      has_number = true;
+    } else {
+      if (!has_number) {
+        return false;
+      }
+
+      while (str[i] === ' ') {
+        i++;
+      }
+
+      if (str[i] !== undefined) {
+        return false;
+      }
+
+      return true;
+    }
+    i++;
+  }
+
+  return has_number;
 }
 
