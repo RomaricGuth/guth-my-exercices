b145f7238a043f41cee6a92b48ca1046c8178e89
romaric.guth <romaric.guth@epita.fr>
2021-10-20 14:35:32 +0200

    exercice 6

--- day-1/exercice-6.js
+++ day-1/exercice-6.js
@@ -1,13 +1,13 @@
-export const my_size_alpha = (str) => {
-  if (typeof str !== 'string') {
+export const my_length_array = (arr) => {
+  if (!Array.isArray(arr)) {
     return 0;
   }
 
   let size = 0;
-  while (str[size] !== undefined) {
+  while (arr[size] !== undefined) {
     size++;
   }
   return size;
 }
 
-console.log(my_size_alpha('6chars'));
+console.log(my_length_array([1, 2, 'a']));
