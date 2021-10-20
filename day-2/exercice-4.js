09a4edbe34b29beab39790ff3328f6b0a2524609
romaric.guth <romaric.guth@epita.fr>
2021-10-20 15:40:51 +0200

    exercice 2-4

---
+++ day-2/exercice-4.js
@@ -0,0 +1,11 @@
+export const my_display_comb = () => {
+    const res = new Array(100 * 100).fill(0).map((_, index) => {
+        const nb1 = Math.floor(index / 100);
+        const nb2 = index - nb1 * 100;
+        return `${nb1} ${nb2}`;
+    });
+    return res;
+}
+
+console.log(my_display_comb());
+console.log(my_display_comb().slice(490));
